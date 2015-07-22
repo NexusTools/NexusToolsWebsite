414bc7c5d3d0e5647706edb0544049a44236aed7
Katelyn Slater <ktaeyln@gmail.com>
2015-07-22 16:05:14 -0400

    Fixed responsiveness Added in bootstrap source

---
+++ sass.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sass --style compressed src/sources/css/bootstrap.scss src/static/css/bootstrap.min.css
+sass --style compressed src/sources/css/nexustools.scss src/static/css/nexustools.min.css
